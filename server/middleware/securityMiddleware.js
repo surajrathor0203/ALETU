2f2fd3ae7d24d2bac6bab537ed2a1ab8d813187a
suraj rathor <suraj22bcs51@iiitkottayam.ac.in>
2024-12-29 15:31:50 +0530

    basic registratoin and login

---
+++ server/middleware/securityMiddleware.js
@@ -0,0 +1,14 @@
+
+const cors = require('cors');
+
+const securityMiddleware = (app) => {
+  app.use(cors());
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+};
+
+module.exports = securityMiddleware;
